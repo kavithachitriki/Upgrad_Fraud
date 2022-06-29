7465d487a40532be39d81bd7a6d0be78ce7d9496
kavithabp <kavitha@silverminegroup.com>
2022-06-29 15:06:45 +0530

    Revert "test revert"
    This reverts commit cd952f48bc9eef19238d17f0260e0f203cd7a683.

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,2 @@
 print("Adding all data processing code related to fraud  detection")
-
-a=5
-b=8
-
-def sum(a,b):
-    print(a+b)
+print("Adding all data processing code related to fraud  detection")
